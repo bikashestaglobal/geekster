9f74974bd17a63caa8ed9cdc859f2637d1c422cf
Bikash Kumar Singh <bikashsinghak47@gmail.com>
2023-02-18 23:34:08 +0530

    validation project initialized

---
+++ valiation/src/main/java/com/geekster/valiation/ValiationApplication.java
@@ -0,0 +1,13 @@
+package com.geekster.valiation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValiationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValiationApplication.class, args);
+	}
+
+}
---
+++ valiation/src/test/java/com/geekster/valiation/ValiationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekster.valiation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValiationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
